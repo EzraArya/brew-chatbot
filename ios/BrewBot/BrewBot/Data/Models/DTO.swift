d36d99ba1677b840a083f86772f05c2cd10da096
EzraArya <ezrawijaya10@gmail.com>
2026-06-20 23:50:21 +0700

    Create DTO.swift

---
+++ ios/BrewBot/BrewBot/Data/Models/DTO.swift
@@ -0,0 +1,38 @@
+//
+//  DTO.swift
+//  BrewBot
+//
+//  Created by Ezra Arya Wijaya on 20/06/26.
+//
+
+import Foundation
+
+struct MessageDTO: Codable {
+    let role: String
+    let content: String
+}
+
+struct ChatRequestDTO: Codable {
+    let history: [MessageDTO]
+    let userMessage: String
+}
+
+struct ChatResponseDTO: Codable {
+    let reply: String
+}
+
+extension MessageDTO {
+    init(from message: Message) {
+        self.role = message.role.rawValue
+        self.content = message.content
+    }
+}
+
+extension Message {
+    init(from dto: MessageDTO) {
+        self.id = UUID()
+        self.role = dto.role == "user" ? .user : .model
+        self.content = dto.content
+        self.timestamp = Date()
+    }
+}
