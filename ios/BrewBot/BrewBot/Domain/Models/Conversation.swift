69e0c77e0dcaab2ffd16df4c3936fbc2ff1ea457
EzraArya <ezrawijaya10@gmail.com>
2026-06-21 12:23:44 +0700

    Create ChatRepositoryProtocol.swift

---
+++ ios/BrewBot/BrewBot/Domain/Models/Conversation.swift
@@ -0,0 +1,24 @@
+//
+//  Conversation.swift
+//  BrewBot
+//
+//  Created by Ezra Arya Wijaya on 21/06/26.
+//
+
+import Foundation
+
+struct Conversation: Identifiable, Equatable, Sendable, Codable {
+    let id: UUID
+    var title: String
+    var messages: [Message]
+    let createdAt: Date
+    var updatedAt: Date
+
+    init(title: String = "New Conversation", messages: [Message] = []) {
+        self.id = UUID()
+        self.title = title
+        self.messages = messages
+        self.createdAt = Date()
+        self.updatedAt = Date()
+    }
+}
